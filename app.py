75dcb6a9e81eec4770c3f2da863bce4c801854ef
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-01-16 14:45:09 +0700

    The initial part of backend

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/login")
+@app.route("/authorization")
+def registration():
+    return render_template("authorization.html")
+
+
+@app.route("/registration")
+def login():
+    return render_template("registration.html")
+
+
+@app.route("/password-recovery")
+def password_recovery():
+    return render_template("password-recovery.html")
+
+
+@app.route("/profile")
+def account():
+    return render_template("profile.html")
+
+
+if __name__ == "__main__":
+      app.run(debug=True)
