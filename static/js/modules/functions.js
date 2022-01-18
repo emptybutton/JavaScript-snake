6e5cec8b73bd74ee0098d57134ab957e626cdad6
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-01-18 19:19:10 +0700

    Cleansing from unnecessary functions

--- static/js/modules/functions.js
+++ static/js/modules/functions.js
@@ -1,14 +1,3 @@
-export function functionStructuring(func, ...funcArguments) {
-  return () => func(...funcArguments);
-}
-
-
-export function activateEverybodyFrom(objectsVisibility) {
-  for (let i = 0; i < objectsVisibility.length; i++) {
-    objectsVisibility[i].event();
-  }
-}
-
 export function convertToCssRGBA(array) {
   return `rgba(${array.slice(0, 4)})`;
 }
