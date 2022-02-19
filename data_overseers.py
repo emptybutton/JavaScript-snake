fab5cc25ff872afe4543fbbd9cc78dec6bc5e5e9
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-02-19 17:32:11 +0700

    A special response class for the caretaker that stores the result of the check and additional information

--- data_overseers.py
+++ data_overseers.py
@@ -7,6 +7,20 @@ class Overseer:
     """An abstract class responsible for the correctness of data implemented through interfaces and storing their configuration"""
 
 
+class OverseerResponse:
+    """Overseer message about data correctness"""
+
+    def __init__(self, sign: bool, message: str = ""):
+        self.sign = sign
+        self.message = message
+
+    def __bool__(self) -> bool:
+        return self.sign
+
+    def __str__(self) -> str:
+        return self.message
+
+
 class IOverseer:
     """Interface containing methods for determining data correctness and their default configuration"""
 
@@ -17,17 +31,17 @@ class IUserDataOverseer(IOverseer):
     USER_PASSWORD_SIZE = [1, infinity]
     FORBIDDEN_LETTERS_FOR_USER_URL = []
 
-    def is_username_correct(self, username) -> bool:
         return validators.length(username, min=self.USERNAME_SIZE[0], max=self.USERNAME_SIZE[1])
-
-    def is_user_email_correct(self, user_email) -> bool:
         return all([validators.email(user_email), validators.length(user_email.split("@")[0], min=self.USER_EMAIL_SIZE[0], max=self.USER_EMAIL_SIZE[1])])
-
-    def is_user_password_correct(self, user_password) -> bool:
         return validators.length(user_password, min=self.USER_PASSWORD_SIZE[0], max=self.USER_PASSWORD_SIZE[1])
-
-    def is_user_url_correct(self, user_url) -> bool:
         return all(map(lambda letter: not letter in self.FORBIDDEN_LETTERS_FOR_USER_URL, user_url))
+    def is_username_correct(self, username) -> OverseerResponse:
+
+    def is_user_email_correct(self, user_email) -> OverseerResponse:
+
+    def is_user_password_correct(self, user_password) -> OverseerResponse:
+
+    def is_user_url_correct(self, user_url) -> OverseerResponse:
 
 
 class MainOverseer(Overseer, IUserDataOverseer):
