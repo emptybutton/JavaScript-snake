735d2d914b68740b9e3c5b8dd7723f566aabc500
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-02-24 00:28:48 +0700

    Redesigned authorization page

--- static/js/authorization.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById("account-form")
-form.addEventListener("submit", accountLogin);
-
-
-function accountLogin(event) {
-  let name = form.accountLogin.value;
-  let pass = form.accountPassword.value;
-}
